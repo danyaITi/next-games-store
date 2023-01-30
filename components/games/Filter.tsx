b1c2c84fb638d367b7cff5d9a8fcefec5e0ff555
danyaITi <danil.lenskiy.1997@mail.ru>
2023-01-31 01:36:29 +0300

    Написана функция подсчитываюшая count выбранных фильтров

--- components/games/Filter.tsx
+++ components/games/Filter.tsx
@@ -5,14 +5,22 @@ import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import styles from '../../styles/components/filter.module.scss';
 import { SelectItem } from '../../types/types';
+import React, { useState } from 'react';
 
 interface SelectProps {
     data:SelectItem
+    changeCount:()=>void
     
 }
-const Select:React.FC<SelectProps> = ({data}) => {
+const Select:React.FC<SelectProps> = ({data,changeCount}) => {
+    const [active,setActive] = useState(false)
+    const {title, items} = data;
 
-    const {title, items} = data
+    const qwe = () => {
+        changeCount()
+        setActive(true)
+    }
+
 
     return(
         <Accordion className={styles.filter}>
@@ -21,7 +29,6 @@ const Select:React.FC<SelectProps> = ({data}) => {
             aria-controls="panel1a-content"
             id="panel1a-header"
             className={styles.block}
-             
             >
                 <Typography className={styles.name}>{title}</Typography>
             </AccordionSummary>
@@ -29,7 +36,10 @@ const Select:React.FC<SelectProps> = ({data}) => {
             <AccordionDetails>
                 <Typography className={styles.selectItems}>
                     {items.map((item)=>(
-                        <span key={item.id}>{item.name}</span>
+                        <span key={item.id}>
+                            <input type='checkbox' onClick={qwe}/>
+                            <label>{item.name}</label>
+                        </span>
                     ))}
                 </Typography>
             </AccordionDetails>
--- pages/games.tsx
+++ pages/games.tsx
@@ -8,6 +8,7 @@ import Select from '../components/games/Filter';
 import {selectGenre, selectPrice} from '../utils/selectItems';
 import DrawerComponent from '../components/games/Drawer';
 import Search from '../components/games/Search';
+import { useState } from 'react';
 
 export const getStaticProps: GetStaticProps = async () => {
     try {
@@ -33,6 +34,12 @@ export const getStaticProps: GetStaticProps = async () => {
 }
 
 const Games:React.FC<Games> = ({games}) => {
+    const [count, setCount] = useState<number>(0);
+
+    const changeCount = () => {
+        setCount(prevCount => prevCount+1)
+    };
+
 
     return(
         <Container maxWidth="lg">
@@ -47,12 +54,12 @@ const Games:React.FC<Games> = ({games}) => {
 
                 <div className={styles.right}>
                     <div className={styles.count}>
-                        <span>Фильтры (1)</span>
+                        <span>Фильтры ({count})</span>
                         <span>Cбросить</span>
                     </div>
                     <Search/>
-                    <Select data={selectPrice}/>
-                    <Select data={selectGenre}/>
+                    <Select changeCount={changeCount} data={selectPrice}/>
+                    <Select changeCount={changeCount} data={selectGenre}/>
                 </div>
             </Box>
         </Container>
